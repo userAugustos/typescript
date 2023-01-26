29a098df2c6a1e65a2f77aa9c10e1302882ca6eb
Felipe Augustos <felipe_augustos84@outlook.com.br>
2023-01-25 22:42:03 -0300

    fix: assertions errors, now all good

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 const names = ['Felipe', 'Brenda', 'Luca']
 
-names.forEach(s => name+='pi');
+names.forEach(s => s+='pi');
 
 console.debug(names);
 
 function treatObject(obj: { x: string, z: string }) {
-    console.debug(pt.x)
-    console.debug(pt.z)
+    console.debug(obj.x)
+    console.debug(obj.z)
 }
 
 treatObject({ x: 'z', z: 'x' })
@@ -30,5 +30,25 @@ type Animal = {
 
 type Human = Animal & {
     mammal: boolean
+}
+
+// Assertions
+function logSomething<T>(): T {
+    console.log("Something")
+    return {} as T
+}
+
+const objectList = logSomething() as Record<string,any>[]
+
+// or 
+const text = logSomething<string>()
+
+// but we still can use generics here:
+let expr = "algo"
+
+function extendingWithTypeInside<T>(): [T] {
+    const list = (expr as any) as T //another simple example, but this i don't recomend
+
+    return [list]
 }
 
